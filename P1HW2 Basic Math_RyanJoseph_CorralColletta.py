d620f661a9630039c946c7b4dfd15273395d029b
corralcr0862 <78762038+corralcr0862@users.noreply.github.com>
2021-02-27 20:16:37 -0500

    Add files via upload
    expression work also late

---
+++ P1HW2 Basic Math_RyanJoseph_CorralColletta.py
@@ -0,0 +1,14 @@
+num_1 = int(input('\nEnter your first number '))
+print(num_1)
+
+num_2 = int(input('\nEnter your second number '))
+print(num_2)
+
+print(num_2 + num_1,end=' ')
+
+print(' is your number added, now lets multiply')
+
+print(' \n BAM POW SHAM WOW their is the two numbers multiplied -->',end=' ')
+
+print(num_2 * num_1,end=' ' )
+
