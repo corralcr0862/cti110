51bdfb693ac304661308902254ec7f1375088f2a
corralcr0862 <78762038+corralcr0862@users.noreply.github.com>
2021-03-03 14:58:37 -0500

    Add files via upload
    distance traveled assignment

---
+++ P2HW1_DistanceTraveled_RyanJosephCorralColletta.py
@@ -0,0 +1,16 @@
+# Car travel distance
+# 3/3/2021
+# CTI-110 - DistanceTraveled
+# RyanJosephCorralColletta
+#
+
+
+Speed = float(input('\nEnter your car speed '))
+print('your speed is -->', Speed)
+
+Time = float(input('\nEnter your hours traveled '))
+print('your hours traveled is -->',Time)
+
+distance_traveled = Speed * Time
+
+print('\n your traveled distance is -->',distance_traveled )
