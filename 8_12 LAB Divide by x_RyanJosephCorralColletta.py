3078161926d2e20247376795d513fd323040b7a8
corralcr0862 <78762038+corralcr0862@users.noreply.github.com>
2021-03-03 15:15:55 -0500

    Add files via upload
    lab divide x due work (:

---
+++ 8_12 LAB Divide by x_RyanJosephCorralColletta.py
@@ -0,0 +1,12 @@
+num_1 = int(input('\nwhat do you want to divide? '))
+print(num_1)
+
+num_2 = int(input('\nwhat are you dividing by? '))
+print(num_1)
+
+num_3 = num_1//num_2
+
+num_4 = num_3//num_2
+
+num_5 = num_4//num_2
+print (num_3,num_4,num_2)
